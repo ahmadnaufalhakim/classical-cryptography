d20448562f76e712fe7ddf8cf17e9d8d25382e16
sy-anwar <syaiful.anwar11111@gmail.com>
2021-02-05 06:34:58 +0700

    update full vigenere

--- src/full_vigenere_cipher.py
+++ src/full_vigenere_cipher.py
@@ -5,67 +5,59 @@ import re
 class FullVigenereCipher:
 	def __init__(self):
 		self.alphabet_uppercase = list(string.ascii_uppercase)
-		self.full_vigenere_array = []
+		self.full_vigenere_matrix = self._generate_full_vigenere_matrix()
 		self.regex = re.compile('[^a-zA-Z]')
 
-	def post_process_space(self, text, spaces=0):
-		text = [text[i-spaces:i] for i in range(spaces, len(text)+spaces, spaces)]
+	def _post_process(self, text):
+		text = [text[i:i+5] for i in range(0, len(text), 5)]
 		text = " ".join(text)
 		return text
 
-	def _generate_full_vigenere_table(self):
-		self.full_vigenere_array = []
+	def _generate_full_vigenere_matrix(self):
+		matrix = []
 		for x in range(26):
 			is_duplicate = True
 			while is_duplicate:
 				temp_alphabet = self.alphabet_uppercase
 				random.shuffle(temp_alphabet)
 				temp_string = ''.join(temp_alphabet)
-				if temp_string not in self.full_vigenere_array :
+				if temp_string not in matrix :
 					is_duplicate = False
-			self.full_vigenere_array.append(temp_string)
+			matrix.append(temp_string)
+		
+		return matrix
 
-	def encrypt(self, plaintext, key, spaces=0):
+	def encrypt(self, plaintext, key, spacing=False):
 		plaintext = self.regex.sub('', plaintext.lower())
 		key = self.regex.sub('', key.lower())
-		self._generate_full_vigenere_table()
-		key_len = len(key)
-		result = ''
+		ciphertext = ''
 
-		for idx_plaintext in range(len(plaintext)):
-			idx_key = idx_plaintext % key_len
-			vigenere_column = string.ascii_lowercase.index(plaintext[idx_plaintext])
-			vigenere_row = string.ascii_lowercase.index(key[idx_key])
+		for i in range(len(plaintext)):
+			idx_key = i % len(key)
+			col = string.ascii_lowercase.index(plaintext[i])
+			row = string.ascii_lowercase.index(key[idx_key])
 			
-			result += self.full_vigenere_array[vigenere_row][vigenere_column]
+			ciphertext += self.full_vigenere_matrix[row][col]
 		
-		if spaces:
-			result = self.post_process_space(result, spaces)
+		if spacing:
+			return self._post_process(ciphertext)
 
-		return result
+		return ciphertext
 
-	def decrypt(self, ciphertext, key, spaces=0):
+	def decrypt(self, ciphertext, key, spacing=False):
 		ciphertext = self.regex.sub('', ciphertext.upper())
 		key = self.regex.sub('', key.lower())
-		key_len = len(key)
-		result = ''
+		plaintext = ''
 
-		for idx_ciphertext in range(len(ciphertext)):
-			idx_key = idx_ciphertext % key_len
-			idx_row = string.ascii_lowercase.index(key[idx_key])
-			vigenere_row = self.full_vigenere_array[idx_row]
-			alphabet_idx = vigenere_row.index(ciphertext[idx_ciphertext])
+		for i in range(len(ciphertext)):
+			idx_key = i % len(key)
+			row = string.ascii_lowercase.index(key[idx_key])
+			vigenere_row = self.full_vigenere_matrix[row]
+			letter_idx = vigenere_row.index(ciphertext[i])
 			
-			result += string.ascii_lowercase[alphabet_idx]
+			plaintext += string.ascii_lowercase[letter_idx]
 		
-		if spaces :
-			result = self.post_process_space(result, spaces)
+		if spacing :
+			return self._post_process(plaintext)
 
-		return result
-
-# fv = FullVigenere()
-# ciphertext = fv.encrypt('Kr9y_ ptOcobacoba', 'coba.', 5)
-# print(ciphertext)
-# print(fv.full_vigenere_array)
-# plaintext = fv.decrypt(ciphertext, 'coba.', 5)
-# print(plaintext)
+		return plaintext
