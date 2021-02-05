b1c4d6372557ff7f76b08133596a680162adad8d
sy-anwar <syaiful.anwar11111@gmail.com>
2021-02-05 07:32:00 +0700

    remove generate key

--- src/extended_vigenere_cipher.py
+++ src/extended_vigenere_cipher.py
@@ -4,25 +4,20 @@ class ExtendedVigenereCipher:
     def __init__(self):
         self.key = ""
 
-    def _generate_key(self, text):
-        if len(self.key) >= len(text):
-            return
-        key = self.key
-        for i in range(len(text) - len(key)):
-            self.key += key[i % len(key)]
-
     def _post_process(self, text):
         text = [text[i:i+5] for i in range(0, len(text), 5)]
         return " ".join(text)
 
     def encrypt(self, plaintext, key, spacing=False):
         self.key = key
-        self._generate_key(plaintext)
 
         ciphertext = []
-        for i in range(len(plaintext)):
-            ch = (ord(plaintext[i]) + ord(self.key[i])) % 256
-            ciphertext.append(ch)
+        i = 0
+        for char in plaintext:
+            encrypted_char = (ord(char) + ord(self.key[i])) % 256
+            ciphertext.append(encrypted_char)
+            i = (i+1) % len(key)
+
         ciphertext = bytes(ciphertext)
         if spacing:
             return self._post_process(ciphertext)
@@ -31,12 +26,14 @@ class ExtendedVigenereCipher:
 
     def decrypt(self, ciphertext, key, spacing=False):
         self.key = key
-        self._generate_key(ciphertext)
 
         plaintext = []
-        for i in range(len(ciphertext)):
-            ch = (ord(ciphertext[i]) - ord(self.key[i]) + 256) % 256
-            plaintext.append(ch)
+        i = 0
+        for char in ciphertext:
+            decrypted_char = (ord(char) - ord(self.key[i]) + 256) % 256
+            plaintext.append(decrypted_char)
+            i = (i+1) % len(key)
+
         plaintext = bytes(plaintext)
         if spacing:
             return self._post_process(plaintext)
